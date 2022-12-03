47dabddd4e9415f88f5b041e98bff60d3a7f2584
Dariel Arriaga <115731963+DarielArriaga@users.noreply.github.com>
2022-12-02 22:39:07 -0800

    Add files via upload

---
+++ CapstoneProject.cpp
@@ -0,0 +1,29 @@
+
+// It's a rough story, please bear with it
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <windows.h>
+#include "Header1.h"
+#include <chrono>
+#include <thread>
+#include <cstdlib>
+#include <fstream>
+#include <ctime>
+
+
+using namespace std;
+
+
+
+
+int main()
+{
+
+    Story();
+    
+
+
+	
+
+}
